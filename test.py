898208cc244c314e446053ea5da201d822618b57
szyszymon <szymoneiro@gmail.com>
2021-06-17 16:53:38 +0200

    Adding files tree

---
+++ test.py
@@ -0,0 +1,22 @@
+import csv
+import json
+from pathlib import Path
+from typing import Dict, List
+
+def load_json(file_content):
+    m.lower().endswith(('.json'))
+    pass
+
+def load_csv(file_content):
+
+    pass
+
+def load_file(file_path): #ma jedynie używać metodę load_csv/json
+# rozbij stringa na części i sprawdź rozszerzenie pliku
+    pass
+
+# przekaż bajty (mają się nazywać się file_content) i przekaż do metody load_csv/load_json
+
+load_file("participiants\participiants1.csv")
+
+
