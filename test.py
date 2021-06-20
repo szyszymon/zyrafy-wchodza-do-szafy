c93d77855ae670038df8aa40955eea6ec7a57674
szyszymon <szymoneiro@gmail.com>
2021-06-20 21:50:32 +0200

    First attempt for open files

--- test.py
+++ test.py
@@ -3,20 +3,38 @@ import json
 from pathlib import Path
 from typing import Dict, List
 
-def load_json(file_content):
-    m.lower().endswith(('.json'))
-    pass
+file_path = "./upskill/recipients"
 
-def load_csv(file_content):
+def read_json(file_path):
+   json_file = open(file_path, "r")
+      # data = json.load(json_file)
+      # print("Type:", type(data))
+   print(json.load(json_file))
 
-    pass
+# def load_csv(file_path):
+#    with open(file_path, 'r') as csv_file:
+#       reader = csv.DictReader(csv_file, skipinitialspace=True)
+#       return row in csv.DictReader
 
-def load_file(file_path): #ma jedynie używać metodę load_csv/json
+#ma jedynie używać metodę load_csv/json
 # rozbij stringa na części i sprawdź rozszerzenie pliku
-    pass
+def load_file(file_path):
+   extension = file_path.suffix.lower()
+   # if extension == ".csv":
+   #    return read_csv(file_path)
+   if extension == ".json":
+      return read_json(file_path)
+   else:
+      print("Invalid file type. Please provide csv or json.")
+pass
+
 
 # przekaż bajty (mają się nazywać się file_content) i przekaż do metody load_csv/load_json
 
-load_file("participiants\participiants1.csv")
+# load_file("participiants\participiants1.csv")
 
-
+# jak zamiana pliku .csv w dicta ma finalnie wyglądać:
+# loaded_participants = [
+#    dict(id = 1, firstname = "Tanny", last_name = "Bransgrove")
+# ]
+# pass
